547893518bf511d24a4abc7a286a60394a353fe1
tsuchida kento <64092999+kenken-creater@users.noreply.github.com>
2022-06-20 21:32:06 +0900

    Day71

--- codes/Day71/167-Two-Sum-II---Input-Array-Is-Sorted.py
+++ codes/Day71/167-Two-Sum-II---Input-Array-Is-Sorted.py
@@ -9,18 +9,18 @@ class Solution:
 
     ### Someones's solution that helped me learn about two-pointers
     def twoSum(self, numbers, target):
-            """
-            :type numbers: List[int]
-            :type target: int
-            :rtype: List[int]
-            """
-            l = 0
-            r = len(numbers) - 1
-            while l < r:
-                if numbers[l] + numbers[r] == target:
-                    return [l+1, r+1]
-                elif numbers[l] + numbers[r] > target:
-                    r -= 1
-                else:
-                    l += 1
-            return
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l = 0
+        r = len(numbers) - 1
+        while l < r:
+            if numbers[l] + numbers[r] == target:
+                return [l+1, r+1]
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                l += 1
+        return
